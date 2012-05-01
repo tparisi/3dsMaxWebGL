00a6fe1b055176c677da5d609f7294e6960c7bc8
Tony Parisi <tparisi@gmail.com>
2012-04-30 20:43:22 -0700

    Fixed crazy sene laoder but

---
+++ exports/BMW7/2tris.js
@@ -0,0 +1,43 @@
+{
+          "scale" : 1.0,
+          "materials" : [
+
+            {
+              "DbgColor": 16777215,
+              "DbgIndex": 0,
+              "DbgName": "wire_bmw7_0",
+              "colorAmbient": [0,0,0],
+              "colorDiffuse": [0,0,1],
+              "colorSpecular": [0,0,0],
+              "specularCoef": 0,
+              "transparency": 1.0,
+              "vertexColors": false
+            },
+            {
+              "DbgColor": 0,
+              "DbgIndex": 1,
+              "DbgName": "wire_bmw7_1",
+              "colorAmbient": [0,0,0],
+              "colorDiffuse": [0,0,0],
+              "colorSpecular": [0,0,0],
+              "specularCoef": 0,
+              "transparency": 1.0,
+              "vertexColors": false
+            }
+		],
+          "metadata" : { "formatVersion" : 3 },
+        "vertices" : [
+          0,0,0, 50,0,0, 50,50,0, 
+          50,50,0, 0,50,0, 0,0,0 
+		],
+        "normals" : [
+          0,0,1, 0,0,1, 0,0,1, 
+		  0,0,1, 0,0,1, 0,0,1
+		],
+        "uvs" : [[
+
+        ]],
+        "faces" : [
+          34, 0,1,2, 0, 0,1,2,
+		  34, 3,4,5, 1, 3,4,5]
+}
