695f28465e3f44e8e6230067b243f5b2b9757954
Tony Parisi <tparisi@gmail.com>
2012-12-06 18:00:27 -0800

    SB SpotLight Work

--- previewer/libs/sb.js
+++ previewer/libs/sb.js
@@ -2883,7 +2883,7 @@ SB.Light.prototype.update = function()
 {
 	if (this.object)
 	{
-		this.object.color = this.color;
+		this.object.color.setHex(this.color);
 		this.object.intensity = this.intensity;
 	}
 	
@@ -3864,7 +3864,7 @@ SB.DirectionalLight.prototype.update = function()
 	var worldmat = this.object.parent.matrixWorld;
 	worldmat.multiplyVector3(this.position);
 	worldmat.multiplyVector3(this.object.target.position);
-	SB.SceneComponent.prototype.update.call(this);
+	SB.Light.prototype.update.call(this);
 }
 
 /**
@@ -4605,6 +4605,65 @@ SB.RigidBodyBox2D.prototype.applyForce = function(x, y, z)
     this.body.ApplyImpulse(force, position);
 //    this.body.ApplyForce(force, position);
 }
+goog.provide('SB.SpotLight');
+goog.require('SB.Light');
+
+SB.SpotLight = function(param)
+{
+	param = param || {};
+	this.direction = param.direction || new THREE.Vector3(0, 0, -1);
+	this.distance = ( param.distance !== undefined ) ? param.distance : SB.SpotLight.DEFAULT_DISTANCE;
+	this.angle = ( param.angle !== undefined ) ? param.angle : SB.SpotLight.DEFAULT_ANGLE;
+	this.exponent = ( param.exponent !== undefined ) ? param.exponent : SB.SpotLight.DEFAULT_EXPONENT;
+	this.castShadows = ( param.castShadows !== undefined ) ? param.castShadows : SB.SpotLight.DEFAULT_CAST_SHADOWS;
+	
+	SB.Light.call(this, param);
+}
+
+goog.inherits(SB.SpotLight, SB.Light);
+
+SB.SpotLight.prototype.realize = function() 
+{
+	this.object = new THREE.SpotLight(this.color, this.intensity, this.distance, this.angle, this.exponent);
+	this.targetPos = this.position.clone();
+	this.targetPos.addSelf(this.direction.multiplyScalar(SB.Light.DEFAULT_RANGE));	
+	this.object.target.position.copy(this.targetPos);
+
+	if (this.castShadows)
+	{
+	}
+	
+	SB.Light.prototype.realize.call(this);
+}
+
+SB.SpotLight.prototype.update = function() 
+{
+	if (this.object)
+	{
+		// D'oh Three.js doesn't seem to transform light directions automatically
+		// Really bizarre semantics
+		this.targetPos.copy(this.position);
+		this.targetPos.addSelf(this.direction.multiplyScalar(SB.Light.DEFAULT_RANGE));	
+		this.object.target.position.copy(this.targetPos);
+		
+		var worldmat = this.object.parent.matrixWorld;
+		worldmat.multiplyVector3(this.position);
+		worldmat.multiplyVector3(this.object.target.position);
+		
+		// Copy other values
+		this.object.distance = this.distance;
+		this.object.angle = this.angle;
+		this.object.exponent = this.exponent;
+	}
+	
+	// Update the rest
+	SB.Light.prototype.update.call(this);
+}
+
+SB.SpotLight.DEFAULT_DISTANCE = 0;
+SB.SpotLight.DEFAULT_ANGLE = Math.PI / 2;
+SB.SpotLight.DEFAULT_EXPONENT = 10;
+SB.SpotLight.DEFAULT_CAST_SHADOWS = false;
 /**
  * @fileoverview A wire grid floor plane
  * @author Tony Parisi
@@ -6659,6 +6718,7 @@ goog.require('SB.Mouse');
 goog.require('SB.Picker');
 goog.require('SB.Light');
 goog.require('SB.DirectionalLight');
+goog.require('SB.SpotLight');
 goog.require('SB.Loader');
 goog.require('SB.NetworkClient');
 goog.require('SB.PhysicsBody');
